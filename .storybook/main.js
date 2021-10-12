e0a1ee3105f548b2ba5f4284690bbf62e4fff351
Chris Maxwell <chmaxwel@icloud.com>
2021-10-12 14:57:38 +0200

    update to storybook-docs build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,11 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // Changes the load order of our stories. First loads the Intro page
+  // automatically import all files ending in *.stories.js|mdx
+  stories: [
+    +"../src/Intro.stories.mdx",
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
