ab63213fc90d5d8ed6ce972c30e8e61e5bbe3328
Chris Maxwell <chmaxwel@icloud.com>
2021-10-13 12:05:30 +0200

    removed stories and update to Avatar

--- src/Link.test.js
+++ src/Link.test.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { Link } from "./Link";
+import { Link } from './Link';
 
 /**
  * A straightforward link wrapper that renders an <a> with the passed props.
  * What we are testing here is that the Link component passes the right props to the wrapper and itself.
  */
-const LinkWrapper = (props) => <a {...props} />; // eslint-disable-line jsx-a11y/anchor-has-content
+const LinkWrapper = props => <a {...props} />; // eslint-disable-line jsx-a11y/anchor-has-content
 
-it("has a href attribute when rendering with linkWrapper", () => {
-  const div = document.createElement("div");
+it('has a href attribute when rendering with linkWrapper', () => {
+  const div = document.createElement('div');
   ReactDOM.render(
     <Link href="https://storybook.js.org/tutorials/" LinkWrapper={LinkWrapper}>
       Link Text
@@ -18,10 +18,8 @@ it("has a href attribute when rendering with linkWrapper", () => {
     div
   );
 
-  expect(
-    div.querySelector('a[href="https://storybook.js.org/tutorials/"]')
-  ).not.toBeNull();
-  expect(div.textContent).toEqual("Link Text");
+  expect(div.querySelector('a[href="https://storybook.js.org/tutorials/"]')).not.toBeNull();
+  expect(div.textContent).toEqual('Link Text');
 
   ReactDOM.unmountComponentAtNode(div);
 });
